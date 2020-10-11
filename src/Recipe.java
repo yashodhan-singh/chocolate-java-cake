ae6a31706f532ffe148da28dd69c11dd25413a51
Yashodhan Singh <35937088+yashodhan-singh@users.noreply.github.com>
2020-10-11 12:13:07 -0400

    Add files via upload

---
+++ src/Recipe.java
@@ -0,0 +1,55 @@
+public class Recipe {
+    private int id;
+    private String name;
+    private String description;
+    private String[] ingredients;
+    private String[] instructions;
+    
+    public Recipe(int id, String name, String description, String[] ingredients, String[] instructions) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String[] getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String[] instructions) {
+        this.instructions = instructions;
+    }
+}
